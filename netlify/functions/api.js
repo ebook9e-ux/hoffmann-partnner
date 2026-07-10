9166534acd30030f07abd5e7306465ff0ea17573
Zrar Zarza <shno49@gmail.com>
2026-07-10 02:21:18 +0200

    test netlify api

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,4 +1,18 @@
+/*
 const serverless = require("serverless-http");
 const app = require("../backend/server");
+
+module.exports.handler = serverless(app);
+*/
+const serverless = require("serverless-http");
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "API is working"
+  });
+});
 
 module.exports.handler = serverless(app);
